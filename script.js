73e530d856e720fd9793138a3415df2747e14a3f
V B <veerbajaj11@gmail.com>
2025-05-11 04:52:05 +0000

    ewr

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ const signatureFrom = document.getElementById('signatureFrom');
 signatureFrom.style.fontSize = '1.25rem';
 signatureFrom.style.fontWeight = 'bold';
 signatureFrom.style.color = '#db2777';
-signatureFrom.textContent = isShared ? ('from ' + fromName + ' with love') : 'from Veer and Jiya with love';
+signatureFrom.textContent = isShared ? ('from ' + fromName + ' with love') : 'Veer and Jiya';
 
 // Rickroll logic
 const rickroll = urlParams.get('rickroll') === 'true';
